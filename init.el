e4938cd35672145f4dd1cf1694eb365e4e458f9e
Ashlynn Anderson <pea@pea.sh>
2022-07-07 17:22:06 -0400

    Assimilate no-littering + Assimilate use-package

--- init.el
+++ init.el
@@ -6,3 +6,30 @@
   (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
   (require 'borg)
   (borg-initialize))
+
+(eval-and-compile ;; use-package
+  (require 'use-package)
+  (setq use-package-verbose t)
+  (setq use-package-always-defer t))
+
+(use-package no-littering
+  :demand t
+  :custom
+  (auto-save-file-name-transforms `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  :config
+  (with-eval-after-load 'recentf
+    (add-to-list 'recentf-exclude no-littering-var-directory)
+    (add-to-list 'recentf-exclude no-littering-etc-directory)))
+
+(progn ;; disable customize
+  ;; https://github.com/doomemacs/doomemacs/blob/35865ef5e89442e3809b8095199977053dd4210f/core/core-ui.el#L628-L639
+  (dolist (sym '(customize-option customize-browse customize-group customize-face
+				  customize-rogue customize-saved customize-apropos
+				  customize-changed customize-unsaved customize-variable
+				  customize-set-value customize-customized customize-set-variable
+				  customize-apropos-faces customize-save-variable
+				  customize-apropos-groups customize-apropos-options
+				  customize-changed-options customize-save-customized))
+    (put sym 'disabled nil))
+  (put 'customize-themes 'disabled nil)
+  (setq custom-file nil))
