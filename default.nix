d81aa5f482a7dd99dac4cf290514a4de61c0ac08
Ashlynn Anderson <pea@pea.sh>
2022-08-14 21:38:03 -0400

    Replace emacs submodule with patches

--- default.nix
+++ default.nix
@@ -1,13 +1,20 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
+  pkgs = import sources.nixpkgs {
+    overlays = [
+      (import sources.emacs-overlay)
+    ];
+  };
 in {
   inherit pkgs;
-  emacs = pkgs.emacs.overrideAttrs (old: {
+  emacs = pkgs.emacsGit.overrideAttrs (old: {
     # TODO: figure out how to get version
     name = "emacs-lambdadog";
-    srcRepo = true;
-    src = ./emacs;
+
+    patches = old.patches ++ [
+      ./emacs-patches/0001-Naive-implementation.patch
+      ./emacs-patches/0002-No-truncation-continuation-characters.patch
+    ];
 
     postPatch = old.postPatch + ''
       substituteInPlace lisp/loadup.el \
