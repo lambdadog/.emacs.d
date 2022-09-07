91a0e54cd378e0978f01ce82b8205cfc8060c2e9
Ashlynn Anderson <pea@pea.sh>
2022-09-07 17:33:51 -0400

    custom.el

--- etc/custom.el
+++ etc/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(custom-safe-themes
    '("3a496bf8eafa4dfe4db359b964f559066aafb516898f63fdf450188133112c7b" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" default)))
 (custom-set-faces
@@ -10,4 +11,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((((class color) (min-colors 256)) :inherit mode-line :box (:line-width 4 :color "#ffa4dc"))))
+ '(mode-line-inactive ((((class color) (min-colors 256)) :inherit mode-line-inactive :box (:line-width 4 :color "#efd3e4")))))
