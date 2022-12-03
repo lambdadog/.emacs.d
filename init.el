bd2482dee269732299d640e5757bf2e0dc67aa80
Ashlynn Anderson <pea@pea.sh>
2022-12-02 19:51:07 -0500

    Assimilate epkg and dependencies
    Assimilate emacsql
    Assimilate closql
    Assimilate compat

---
+++ init.el
@@ -0,0 +1,5 @@
+(when (< emacs-major-version 27)
+  (setq package-enable-at-startup nil))
+(add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
+(require 'borg)
+(borg-initialize)
