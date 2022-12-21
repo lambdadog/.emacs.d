3198e7018255f3102816664c545df6f9373b1aff
Ashlynn Anderson <pea@pea.sh>
2022-12-20 21:45:56 -0500

    Orchestrate excludes file
    This should mean I no longer have to touch my global excludes file

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 # TODO for emacs config
 
- - [ ] Orchestrate excludes file
+ - [x] Orchestrate excludes file
    + Using [borg.extra-build-step](https://github.com/emacscollective/borg/issues/129#issuecomment-1340114944)
  - [ ] Evaluate [howm](https://leahneukirchen.org/blog/archive/2022/03/note-taking-in-emacs-with-howm.html)
  - [ ] Evaluate [puni](https://github.com/AmaiKinono/puni)
--- etc/borg/config.el
+++ etc/borg/config.el
@@ -1,2 +1,18 @@
 (when (native-comp-available-p)
   (setq borg-compile-function #'borg-byte+native-compile))
+
+;; TODO: experiment with `git config --local core.excludesFile <path>`
+;; for a path in-repo so we don't need to manually replicate this
+;; everywhere and it's possible to update without rebuilding.
+(defun config/borg-add-exclude-rules (clone)
+  (let ((file (expand-file-name "info/exclude" (borg-gitdir clone))))
+    (message "Populating %s..." file)
+    (ignore-errors
+      (make-directory (file-name-directory file)))
+    (with-temp-file file
+      (insert "*.elc\n")
+      (insert "*-autoloads.el\n")
+      (insert "*.texi\n")
+      (insert "*.info\n")
+      (insert "dir\n"))
+    (message "Populating %s...done" file)))
